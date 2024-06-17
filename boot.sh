95d218c6898f96f729d6b80cac77ed3a1cf58671
belajarit45 <144545476+belajarit45@users.noreply.github.com>
2024-06-17 11:47:01 +0700

    Add files via upload

---
+++ boot.sh
@@ -0,0 +1,16 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg update
+pkg install -y expect curl qemu-utils qemu-common qemu-system-x86_64-headless openssh
+
+
+if [ ! -f "config.env" ]; then
+    # get the default config file
+    cp config.sample config.env
+fi
+
+source config.env
+
+expect -f installqemu.expect
+
+echo "Telah Berhasil Install Linux"
