8790919ccf7629847b7c98483db693000f638697
belajarit45 <144545476+belajarit45@users.noreply.github.com>
2024-06-17 11:53:55 +0700

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,7 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
-
-docker ps
-
-echo "Telah Berhasil Menambahkan Akun"
+boot.expect
